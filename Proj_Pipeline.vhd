ac66ca03ef02943b5f92fdba51b84556ea2f83c4
ThiagoPfeifer09 <166036070+ThiagoPfeifer09@users.noreply.github.com>
2024-12-13 11:00:24 -0300

    Update Proj_Pipeline.vhd

--- Proj_Pipeline.vhd
+++ Proj_Pipeline.vhd
@@ -6,7 +6,9 @@ use ieee.std_logic_arith.all;
 entity Proj_Pipeline is
 	port (
 		clock	    : in std_logic;
-		reset	    : in std_logic
+		reset	    : in std_logic;
+		R1_out      : out std_logic_vector(7 downto 0);
+		R2_out      : out std_logic_vector(7 downto 0)
 		);
 end entity;
 
@@ -15,48 +17,28 @@ architecture behavior of Proj_Pipeline is
     type memoria_instrucao is array (integer range 0 to 255) of std_logic_vector(15 downto 0);
     type banco_regs is array (integer range 0 to 15) of std_logic_vector(7 downto 0);
 
-    signal PC	                                         : std_logic_vector(7 downto 0); 
-    signal regs                                          : banco_regs := (others => (others => '0'));
-    signal desvio	                                     : std_logic; 
-    signal mul, muli                                     : std_logic_vector(15 downto 0); 
-    signal ula                                           : std_logic_vector(7 downto 0); 
-    signal equal	                                     : std_logic;
-    signal R1                                            : std_logic_vector(7 downto 0);
-    signal R2                                            : std_logic_vector(7 downto 0);
-    signal Ri_ID, Ri_EX, Ri_WB                           : std_logic_vector(7 downto 0); 
-    signal PC_ID, PC_EX, PC_MEM, PC_WB                   : std_logic_vector(7 downto 0); 
-    signal inst_ID, inst_EX, inst_MEM, inst_WB           : std_logic_vector(15 downto 0);     
+    signal PC	                                 : std_logic_vector(7 downto 0); 
+    signal regs                                  : banco_regs := (others => (others => '0'));
+    signal desvio	                             : std_logic; 
+    signal mul, muli                             : std_logic_vector(15 downto 0); 
+    signal ula                                   : std_logic_vector(7 downto 0); 
+    signal equal	                             : std_logic;
+    signal R1                                    : std_logic_vector(7 downto 0);
+    signal R2                                    : std_logic_vector(7 downto 0);
+    signal Ri_ID, Ri_EX, Ri_WB                   : std_logic_vector(7 downto 0); 
+    signal PC_ID, PC_EX, PC_MEM, PC_WB           : std_logic_vector(7 downto 0); 
+    signal inst_ID, inst_EX, inst_MEM, inst_WB   : std_logic_vector(15 downto 0);     
     
-    signal mem_inst  	        : memoria_instrucao:= (
-        0  => "0011000000001010", -- BNE R1 != R2 FALSO
-        1  => "0110000000000001", -- LDA endereço 1 para R1 (Valor 1)
-        2  => "0110000100000010", -- LDA endereço 2 para R2 (Valor 3)
-        3  => "1010100000000111", -- LWI Valor 7 no registrador 8
-        4  => "1111111111111111", -- Bolha artificial
-        5  => "1111111111111111", -- Bolha artificial
-        6  => "0100000000011111", -- BEQ R1 = R2 VAI DAR FALSO
-        7  => "0000001000000001", -- ADD R1 + R2 -> R2 => (Valor 4) // hazard de dependência de dados!!
-        8  => "1111111111111111", -- Bolha artificial
-        9  => "1111111111111111", -- Bolha artificial
-        10 => "1000001100100101", -- ADDI R2 + 5 -> R3 => (Valor 9)
-        11 => "1111111111111111", -- Bolha artificial
-        12 => "1111111111111111", -- Bolha artificial
-        13 => "0010010000100011", -- MUL R3 * R2 no R4 (Valor 36)
-        14 => "1011010100010101", -- MUI R2 * 5 no R5 (Valor 15)
-        15 => "0111001000000100", -- STA R2 no endereço 4 (Valor 4)
-        16 => "0011001000010010", -- BNE R2 != R2 SALTO PC+2 => PC 21
-        17 => "0111001100000101", -- STA R3 no endereço 5 (Valor 9)
-        18 => "0010100000100011", -- MUL R3 * R2 no R8 (Valor 36)
-        19 => "1011101000010101", -- MUI R2 * 5 no R20 (Valor 15)
-        20 => "1111111111111111", -- Bolha artificial
-        21 => "0001011000110010", -- SUB R3 - R2 -> R6 (Valor 5)
-        22 => "1001011100100001", -- SUI R2 - 1 -> R7 (Valor 3)
-        23 => "0111010000000100", -- STA R4 no endereço 4 (Valor 36)
-        24 => "0101000000000001", -- JMP para endereço 1
-        25 => "1111111111111111", -- Bolha artificial
-        26 => "1111111111111111", -- Bolha artificial
-        27 => "1111111111111111", -- Bolha artificial
-        others => (others => '1') -- Demais posiçoe zeradas
+    signal mem_inst  	        : memoria_instrucao:= (	
+		0 => "1010000100000110",
+		1 => "1010001000000100",
+		2 => "1111111111111111",
+		3 => "1111111111111111",
+		4 => "1111111111111111",
+		5 => "0000001100010010",
+		6 => "0001010000010010",
+		7 => "1011010100100010",
+	    others => (others => '1') -- Demais posições zeradas
     );
 
     signal mem_dados	            : memoria_dados := ( 
